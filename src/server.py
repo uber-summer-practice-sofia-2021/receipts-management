43dc9f2ebdef55fce50c358c7efbc8f63f3b87ae
Denis Zangarov <denisimo_98@yahoo.com>
2021-09-03 16:43:25 +0300

    pop

--- src/server.py
+++ src/server.py
@@ -32,16 +32,19 @@ urls = ["localhost:8000/get_trip_info", "localhost:5000/get_order_info"]
 #Main function
 @server.route("/receive_trip_id", methods = ['POST'])
 def receiveTripId():
-  tripID = request.get_json()
-  response_from_courier = requests.post("http://localhost:8000/get_trip_info", json=tripID)
+  tripID = request.json
+  server.logger.debug(tripID)
+  #response_from_courier = requests.post("http://localhost:8000/get_trip_info", json=tripID)
 
-  server.logger.debug(response_from_courier.json()['orderId'])
+  #server.logger.debug(response_from_courier)
+  #server.logger.debug(response_from_courier.json()['orderId'])
 
   #server.logger.debug(response.json())
 
   #response.raise_for_status()
   
-  return response_from_courier.json()
+  #return response_from_courier.json()
+  return tripID
   
 
 if __name__ == "__main__":
