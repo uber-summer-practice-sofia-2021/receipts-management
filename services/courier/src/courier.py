a31a94e9fa7430fb52673b7c5482bc4247ce0355
Denis Zangarov <denisimo_98@yahoo.com>
2021-09-04 14:42:59 +0300

    Successful Receiving of whole Trip file from couriers (simulation with endpoints)
    Still more to discuss on the way the communication will be happening.

--- services/courier/src/courier.py
+++ services/courier/src/courier.py
@@ -1,6 +1,7 @@
 import os
 import json
 from flask import Flask, request, Response,  jsonify
+from flask.json import JSONDecoder
 import requests
 
 server = Flask(__name__)
@@ -16,6 +17,15 @@ def sendTripInfo():
   server.logger.debug(file)
   requests.post("http://receipts:5000/receive_trip_id", json = file)
   return jsonify(file)
+
+@server.route("/get_trip_info", methods = ["POST"])
+def sendAllInfo():
+  tripID = request.json
+  server.logger.debug(tripID)
+  file = json.load(open("fixtures/" + tripID+ ".json"))
+  server.logger.debug(file)
+  requests.post("http://receipts:5000/get_trip_info", json = file)
+  return "Successfully Sent"
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
