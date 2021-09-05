7e40a7a7d9412836d2178d1736fb86b23dec1864
Denis Zangarov <denisimo_98@yahoo.com>
2021-09-06 00:16:53 +0300

    Update dockStop.bat

--- dockStop.bat
+++ dockStop.bat
@@ -1,10 +1,7 @@
 docker network disconnect receipt-net couriers
 docker network disconnect receipt-net orders
 docker network disconnect receipt-net receipts
-start "receipts_service_kill" cmd /c "docker compose down" 
-start "orders_service_kill" cmd /c "cd services/orders & docker compose down"
-start "couriers_service_kill" cmd /c "cd services/courier & docker compose down"
+start "receipts_service_kill" cmd /c "docker compose down --rmi local" 
+start "orders_service_kill" cmd /c "cd services/orders & docker compose down --rmi local"
+start "couriers_service_kill" cmd /c "cd services/courier & docker compose down --rmi local"
 docker network rm receipt-net
-docker rmi courier_server
-docker rmi orders_server
-docker rmi receipts-management_server
