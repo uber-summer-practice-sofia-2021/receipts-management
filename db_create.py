94591808b911ea74220dde20a38b61c24a5b7a18
Denis Zangarov <denisimo_98@yahoo.com>
2021-09-07 23:42:21 +0300

    fixed db_create

--- db_create.py
+++ db_create.py
@@ -2,18 +2,18 @@ import sqlite3
 import os
 
 
-database = sqlite3.connect(os.path.dirname(__file__) + "/resources/database/receipts.db")
+database = sqlite3.connect(os.path.dirname(__file__) + "/src/database/receipts.db")
 
 cs = database.cursor()
 
 cs.execute("""CREATE TABLE receipts (
-        receiptId TEXT PRIMARY KEY UNIQUE NOT NULL,
+        receiptId TEXT NOT NULL PRIMARY KEY UNIQUE,
         clientName TEXT NOT NULL,
         clientEmail TEXT NOT NULL,
         phoneNumber TEXT NOT NULL,
-        orderId TEXT UNIQUE NOT NULL,
-        from TEXT NOT NULL,
-        to TEXT NOT NULL,
+        orderId TEXT NOT NULL UNIQUE ,
+        address_from TEXT NOT NULL,
+        address_to TEXT NOT NULL,
         dimensions TEXT,
         deliveryType TEXT NOT NULL,
         tripId TEXT NOT NULL,
