b571ef6eff9b4a511afabc0844eb53976893a898
Denis Zangarov <deniszangarov@yahoo.com>
2021-09-08 10:16:09 +0300

    fixing merge conflict

--- services/courier/src/courier.py
+++ services/courier/src/courier.py
@@ -13,18 +13,16 @@ def hello():
 #Simulates courier API interacting with us
 @server.route("/send", methods=["POST"])
 def sendTripInfo():
-  file = {"tripID":"trip"}
-  server.logger.debug(file)
+  file = {"tripId":"trip"}
   requests.post("http://receipts:5000/receive_trip_id", json = file)
   return "Successfully Sent"
 
 @server.route("/get_trip_info", methods = ["POST"])
 def sendAllInfo():
-  tripID = request.json
-  server.logger.debug(tripID)
-  trip_info = json.load(open(os.path.join(server.root_path, "fixtures", "trip.json")))
+  tripId = request.json
+  with open(os.path.join(server.root_path, "fixtures", tripId + ".json")) as file:
+    trip_info = json.load(file)
   server.logger.debug(trip_info)
-
   return trip_info
 
 if __name__ == "__main__":
