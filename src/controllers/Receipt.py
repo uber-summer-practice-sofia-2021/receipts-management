f07a669b92c33634585015512f84b819f28f8fb1
Denis Zangarov <denisimo_98@yahoo.com>
2021-09-15 00:24:28 +0300

    email_validator (gotta ask about this one)

--- src/controllers/Receipt.py
+++ src/controllers/Receipt.py
@@ -1,12 +1,13 @@
 #from flask import json
 import json
 import uuid
-import numbers
 import re
 
 u_id_validator = re.compile('[\da-f]{8}-[\da-f]{4}-4[\da-f]{3}-[89ab][\da-f]{3}-[\da-f]{12}', flags=re.IGNORECASE)
 phone_validator = re.compile('(\+?359)?[- ]?(((\(0[7-8]00\)|0[7-8]00)[- ]?\d{5})|((\(02\)|02)[- ]?\d{3}[- ]?\d{4})|((\(02\d\)|02\d)[- ]?[- ]?(\d{2}[- ]?\d{4}|\d{3}[- ]?\d{3}))|((\(0?8[4-9]\)|0?8[7-9])[- ]?(\d{5}[- ]?\d{2}|\d{4}[- ]?\d{3}|\d{3}[- ]?\d{4}|\d{2}[- ]?\d{5}|\d[- ]?\d{6}|\d{2}[- ]?\d{3}[- ]?\d{2})))')
 
+#gotta ask about this email validator
+email_validator = re.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*)@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])")
 
 class Receipt:
     template_data = None
@@ -34,8 +35,15 @@ class Receipt:
         if phone_match is None or len(phoneNumber)>20:
             raise Exception('Invalid Phone Number')
 
+    def __checkNumberData(self, decimalNum):
+        try:
+            float(decimalNum)
+        except:
+            raise Exception(decimalNum + ' is not a valid float/int type.')
+
     def __checkString(self, data):
-        return isinstance(data, str)
+        if not isinstance(data, str):
+            raise Exception(data + "is not a string")
 
     def __validateCourier(self, courierResponse):
         self.__checkUUID(courierResponse['courierId'])
