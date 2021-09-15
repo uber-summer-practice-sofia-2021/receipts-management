2beec12de9e5adcee9439fe6300490c8889b94f1
Denis Zangarov <denisimo_98@yahoo.com>
2021-09-15 12:21:23 +0300

    Update Receipt.py

--- src/controllers/Receipt.py
+++ src/controllers/Receipt.py
@@ -4,7 +4,7 @@ import uuid
 import re
 
 u_id_validator = re.compile('[\da-f]{8}-[\da-f]{4}-4[\da-f]{3}-[89ab][\da-f]{3}-[\da-f]{12}', flags=re.IGNORECASE)
-phone_validator = re.compile('(\+?359)?[- ]?(((\(0[7-8]00\)|0[7-8]00)[- ]?\d{5})|((\(02\)|02)[- ]?\d{3}[- ]?\d{4})|((\(02\d\)|02\d)[- ]?[- ]?(\d{2}[- ]?\d{4}|\d{3}[- ]?\d{3}))|((\(0?8[4-9]\)|0?8[7-9])[- ]?(\d{5}[- ]?\d{2}|\d{4}[- ]?\d{3}|\d{3}[- ]?\d{4}|\d{2}[- ]?\d{5}|\d[- ]?\d{6}|\d{2}[- ]?\d{3}[- ]?\d{2})))')
+phone_validator = re.compile('(\+?359[- ]?)?(((\(0?8[4-9]\)|0?8[7-9])[- ]?(\d{5}[- ]?\d{2}|\d{4}[- ]?\d{3}|\d{3}[- ]?\d{4}|\d{2}[- ]?\d{5}|\d[- ]?\d{6}|\d{2}[- ]?\d{3}[- ]?\d{2}))|((\(0[7-8]00\)|0[7-8]00)[- ]?\d{5})|((\(02\)|02)[- ]?\d{3}[- ]?\d{4})|((\(0\d{2}\)|0\d{2})[- ]?[- ]?(\d{2}[- ]?\d{4}|\d{3}[- ]?\d{3}))|((\(0\d{3}\)|0\d{3})[- ]?\d{5})|((\(0\d{4}\)|0\d{4})[- ]?\d{4}))')
 
 #gotta ask about this email validator
 email_validator = re.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*)@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])")
@@ -20,7 +20,7 @@ class Receipt:
 
     def __checkUUID(self, u_id):
         u_id_match = u_id_validator.match(u_id)
-        if u_id_match is None:
+        if u_id_match is None or len(u_id)>36:
             raise Exception('Invalid UUID')
 
     def __checkPhoneNumber(self, phoneNumber):
@@ -31,9 +31,18 @@ class Receipt:
         # 08z xxxx xxx (mobiles)
         # 0800 xxxxx
         # +359-(089)-44-877-03 --------- biggest possible number
-        phone_match = phone_validator.match(phoneNumber)
-        if phone_match is None or len(phoneNumber)>20:
+        try:
+            phone_match = phone_validator.search(phoneNumber).group()
+            return phone_match
+        except:
             raise Exception('Invalid Phone Number')
+
+    def __checkEmail(self, userMail):
+        try:
+            email = email_validator.search(userMail).group()
+            return email
+        except:
+            raise Exception('Invalid E-mail.')
 
     def __checkNumberData(self, decimalNum):
         try:
@@ -52,6 +61,9 @@ class Receipt:
 
     def __validateCourier(self, courierResponse):
         self.__checkUUID(courierResponse['courierId'])
+        self.__checkNumberData(courierResponse['distance'])
+        self.__checkString(courierResponse['courierName'])
+
 
     def __validateOrder(self, orderResponse):
         pass
@@ -65,6 +77,7 @@ class Receipt:
             self.data = orderResponse
         #constructor 2 for creating new receipts from real responses
         else:
+            self.__checkUUID(tripId)
             self.__validateCourier(courierResponse)
             self.__validateOrder(orderResponse)
             self.data = Receipt.template_data
